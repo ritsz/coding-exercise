f80e8f60ae64f9307d717f77ff966406f1bce613
Ritesh Ranjan <r.ranjan789@gmail.com>
2022-09-06 19:21:41 +0530

    Add util classes

---
+++ src/com/coding/exercises/utils/TreeNode.java
@@ -0,0 +1,33 @@
+package com.coding.exercises.utils;
+
+public class TreeNode {
+    
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+}
